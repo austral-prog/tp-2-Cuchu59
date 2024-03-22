00c7ca459811e3c1a047ece397e06e7419d33850
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-03-22 10:39:47 -0400

    Update change.py
    commit 3/3

--- change.py
+++ change.py
@@ -1,3 +1,15 @@
 def change():
     expense = 23.75
     money = 100
+    diff = money - expense
+    
+    pesos = int(diff)
+    centavos = diff - pesos
+    
+    print('')
+    print('VUELTO')
+    print('')
+    print('PESOS:')
+    print(pesos) 
+    print('CENTAVOS:')
+    print(int(centavos * 100))
